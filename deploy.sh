3ed89d83aa019015664738f7b7028f5d7376e074
Bryan Chan <fryanpan@gmail.com>
2021-01-04 22:53:30 -0800

    Increase memory

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ DW_AUTH_TOKEN=`gcloud secrets versions access --secret=data_world_api_token late
 pipenv lock -r > requirements.txt
 gcloud functions deploy process-crdt-data --entry-point run_process_data --runtime python37 \
     --trigger-topic process-crdt-data \
-    --memory 512MB \
+    --memory 1024MB \
     --max-instances 1 \
     --update-env-vars="DW_AUTH_TOKEN=${DW_AUTH_TOKEN}" \
     --timeout 540
